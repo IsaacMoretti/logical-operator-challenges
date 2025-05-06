6a314efb807f60f9ed19982aa8151617589e4c6a
isaac k <isaackunrath@gmail.com>
2025-05-05 21:52:39 -0300

    fifth

--- desafio-or/CheckoutValidation.js
+++
@@ -1,9 +0,0 @@
-const hasDeliveryAddress = true;
-const hasPaymentMethod = true;; 
-const isCartEmpty = true;
-
-if (hasDeliveryAddress && hasPaymentMethod && isCartEmpty) {
-    console.log("Finalize purchase"); 
-} else { 
-    console.log("Please complete your cart, payment method, or shipping address before checkout.");
-}
